32e017928d35ba71f14f32631110fa39373d9f74
KRISHI P <42173275+krishisp13@users.noreply.github.com>
2019-04-17 22:39:06 +0530

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,5 @@
 function validateForm(){
-	/* Loop through fields with the class of "req" */
+	
 	for (var i = 0; i < myform.elements.length; i++) {
         if (myform.elements[i].className == "req" && myform.elements[i].value.length == 0) {
             alert('Please fill in all required fields');
@@ -7,7 +7,7 @@ function validateForm(){
         }
     }
 	
-	/* Make sure the email value is a valid email address */
+	
 	var email  = document.getElementById('email').value;
 	var atpos  = email.indexOf("@");
 	var dotpos = email.lastIndexOf(".");
