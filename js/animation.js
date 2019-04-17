8629d2a45efd8b90abd28a01ec84031c4bce4ec9
KRISHI P <42173275+krishisp13@users.noreply.github.com>
2019-04-17 22:38:17 +0530

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,7 @@
-
+ $(document).ready(function (){
+  $('li.fields').filter(':nth-child(n+4)').addClass('hide');
+  $('ul').on('click','li.title',function(){
+	$(this).next().slideDown(300).siblings('li.fields').slideUp(300);
+  
+  })
+ });   
